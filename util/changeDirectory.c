97f8d0733a31169c905fb7bfd8ccc9ce013f9346
Mich <MichOchieng@gmail.com>
2020-10-06 23:05:07 -0700

    First commit

---
+++ util/changeDirectory.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+
+int main(){  
+     
+    return 0;
+}
