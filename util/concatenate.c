ea51c0777803b9734febef706bb07ab17879aea6
Mich <MichOchieng@gmail.com>
2020-10-07 20:31:50 -0700

    Added basic functionality for cat command

--- util/concatenate.c
+++ util/concatenate.c
@@ -6,7 +6,33 @@
 #include <string.h>
 #include <math.h>
 
+FILE *file;
+void cat(char ** input);
+
 int main(int argc,char ** argv){  
-    printf("This is a test for cat\n");
+    cat(argv);
     return 0;
+}
+
+void cat(char ** input){
+    // Goes through and prints all files given
+    int i = 1;
+    while (input[i] != NULL){
+        file = fopen(input[i],"r");
+        // Checks to see if the file exists
+        if (file == NULL){
+            printf("%s",input[i]);
+            printf(": No such file in directory \n");            
+        }else{
+            // If the file exists loops over the entire file and prints
+            char c = fgetc(file);
+            while(c != EOF){
+                printf("%c",c);
+                c = fgetc(file);
+            }
+            printf("\n");
+        }        
+     i++;   
+    }
+    fclose(file);
 }
